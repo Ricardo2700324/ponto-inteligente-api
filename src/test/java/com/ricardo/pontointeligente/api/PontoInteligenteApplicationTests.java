269f64437e942244092aef54232f8fab7aaa4a01
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2020-02-05 21:13:53 -0300

    Comit inicial dos arquivos do projeto.

---
+++ src/test/java/com/ricardo/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
